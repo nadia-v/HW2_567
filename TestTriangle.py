f22cca486612003d1e8b6b4d0c33e21a66d999f4
Nadia Vedeneyeva <nadiaveden@gmail.com>
2019-02-05 13:16:28 -0500

    Test 01

--- TestTriangle.py
+++ TestTriangle.py
@@ -17,14 +17,35 @@ from Triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def test_classifyTriangle(self): 
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        #Testing expected output
+        self.assertEqual(classifyTriangle(5, 8, 12), 'Scalene')     #Testing scalene triangle 
+        self.assertEqual(classifyTriangle(23456, 23456, 19876),'Isosceles')     #Testing isosceles triangle / large number
+        self.assertEqual(classifyTriangle(1, 1, 1),'Equilateral')     #Testing equilateral triangle 
+        self.assertEqual(classifyTriangle(3, 4, 5),'Right, Scalene')   #Testing right, scalene triangle 
+        self.assertEqual(classifyTriangle(0.5, 0.3, 0.4),'Right, Scalene')   #Testing right, scalene triangle / float
+        self.assertEqual(classifyTriangle(2, 2, (8 ** 0.5)),'Right, Isosceles')   #Testing right, isosceles triangle 
+        self.assertEqual(classifyTriangle(2, (8 ** 0.5), 2),'Right, Isosceles')   #Testing right, isosceles triangle 
+        #Testing invalid input
+        self.assertEqual(classifyTriangle('five', 8, 12), 'InvalidInput')    #Type error
+        self.assertEqual(classifyTriangle(6, 8, '@'), 'InvalidInput')    #Type error
+        self.assertEqual(classifyTriangle(6, 8, 0), 'InvalidInput')    #Zero
+        self.assertEqual(classifyTriangle(0, 0, 0), 'InvalidInput')    #All zeros
+        self.assertEqual(classifyTriangle(6, 1, 2), 'InvalidInput')    #Side larger than the sum of other two
+        self.assertEqual(classifyTriangle(6, 4, 2), 'InvalidInput')    #Side equal to the sum of other two
+        self.assertEqual(classifyTriangle(6, 8, -4), 'InvalidInput')    #Negative number
+        self.assertEqual(classifyTriangle(-3, -4, -5), 'InvalidInput')    #All negative numbers
+
+        #Testing unexpected output
+        self.assertNotEqual(classifyTriangle(5, 8, 12), 'Right, Isosceles')
+        self.assertNotEqual(classifyTriangle(23456, 23456, 19876),'Scalene')    
+        self.assertNotEqual(classifyTriangle(1, 1, 1),'Scalene')     
+        self.assertNotEqual(classifyTriangle(3, 4, 5),'Scalene')   
+        self.assertNotEqual(classifyTriangle(0.5, 0.3, 0.4),'Right, Isosceles')   
+        self.assertNotEqual(classifyTriangle(2, 2, (8 ** 0.5)),'Isosceles')   
+        self.assertNotEqual(classifyTriangle(2, (8 ** 0.5), 2),'Right, Scalene')   
+
 
 if __name__ == '__main__':
     print('Running unit tests')
