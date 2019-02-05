527b3e703cfe3bd4cf0f314755124a930254c67e
Nadia Vedeneyeva <nadiaveden@gmail.com>
2019-02-05 15:14:29 -0500

    test 02

--- TestTriangle.py
+++ TestTriangle.py
@@ -25,15 +25,14 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classifyTriangle(1, 1, 1),'Equilateral')     #Testing equilateral triangle 
         self.assertEqual(classifyTriangle(3, 4, 5),'Right, Scalene')   #Testing right, scalene triangle 
         self.assertEqual(classifyTriangle(0.5, 0.3, 0.4),'Right, Scalene')   #Testing right, scalene triangle / float
-        self.assertEqual(classifyTriangle(2, 2, (8 ** 0.5)),'Right, Isosceles')   #Testing right, isosceles triangle 
-        self.assertEqual(classifyTriangle(2, (8 ** 0.5), 2),'Right, Isosceles')   #Testing right, isosceles triangle 
+        
         #Testing invalid input
         self.assertEqual(classifyTriangle('five', 8, 12), 'InvalidInput')    #Type error
         self.assertEqual(classifyTriangle(6, 8, '@'), 'InvalidInput')    #Type error
         self.assertEqual(classifyTriangle(6, 8, 0), 'InvalidInput')    #Zero
         self.assertEqual(classifyTriangle(0, 0, 0), 'InvalidInput')    #All zeros
-        self.assertEqual(classifyTriangle(6, 1, 2), 'InvalidInput')    #Side larger than the sum of other two
-        self.assertEqual(classifyTriangle(6, 4, 2), 'InvalidInput')    #Side equal to the sum of other two
+        self.assertEqual(classifyTriangle(6, 1, 2), 'NotATriangle')    #Side larger than the sum of other two
+        self.assertEqual(classifyTriangle(6, 4, 2), 'NotATriangle')    #Side equal to the sum of other two
         self.assertEqual(classifyTriangle(6, 8, -4), 'InvalidInput')    #Negative number
         self.assertEqual(classifyTriangle(-3, -4, -5), 'InvalidInput')    #All negative numbers
 
@@ -43,8 +42,7 @@ class TestTriangles(unittest.TestCase):
         self.assertNotEqual(classifyTriangle(1, 1, 1),'Scalene')     
         self.assertNotEqual(classifyTriangle(3, 4, 5),'Scalene')   
         self.assertNotEqual(classifyTriangle(0.5, 0.3, 0.4),'Right, Isosceles')   
-        self.assertNotEqual(classifyTriangle(2, 2, (8 ** 0.5)),'Isosceles')   
-        self.assertNotEqual(classifyTriangle(2, (8 ** 0.5), 2),'Right, Scalene')   
+          
 
 
 if __name__ == '__main__':
--- Triangle.py
+++ Triangle.py
@@ -26,32 +26,50 @@ def classifyTriangle(a,b,c):
       
       BEWARE: there may be a bug or two in this code
     """
-
-    # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
-        return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-      
+    if not(isinstance(a,float) or isinstance(a,int)): 
+        return 'InvalidInput'
+    elif not(isinstance(b,float) or isinstance(b,int)): 
+        return 'InvalidInput'
+    elif not(isinstance(c,float) or isinstance(c,int)): 
+        return 'InvalidInput'
+
+    # require that the input values be > 0     
+    elif a <= 0 or b <= 0 or c <= 0:
+        return 'InvalidInput'
+
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    elif (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
+    right = (((a ** 2) + (b ** 2)) == (c ** 2) or\
+        ((a ** 2) + (c ** 2)) == (b ** 2) or\
+        ((b ** 2) + (c ** 2)) == (a ** 2))
+    if right is True and\
+        a == b and a == c and b == c:
+        return 'Right, Equilateral'
+    elif a == b and a == c and b == c:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
+
+    elif right is True and\
+        a == b and a != c or\
+        a == c and a != b or\
+        b == c and b != a:
+        return 'Right, Isosceles'
+    elif a == b and a != c or\
+        a == c and a != b or\
+        b == c and b != a:
+        return 'Isosceles'
+    
+    elif right is True and\
+        (a != b) and  (b != c) and (a != c):
+        return 'Right, Scalene'
     else:
-        return 'Isoceles'
+        return 'Scalene'
+
